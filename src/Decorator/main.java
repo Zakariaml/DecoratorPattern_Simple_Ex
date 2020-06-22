d719af2a7fe7bc1b57f56ad0b5a8959004d89ff3
Zakaria Miloudi <zakaria.pro.miloudi@gmail.com>
2020-06-22 20:14:14 +0100

    some changes in the main class

--- src/Decorator/main.java
+++ src/Decorator/main.java
@@ -7,8 +7,11 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//FIRST ROW
 		Beverage b = new Caramel(new Choclate(new Milk()));
+
 		System.out.println("The cost of your order is : "+b.cost()+"$");
+		System.out.println("Please change the first row if you want to change the cost");
 		
 	}
 
